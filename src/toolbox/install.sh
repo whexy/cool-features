e5c0607658580a7d19a441860d96c7aeb3161bde
Wenxuan Shi <whexy@outlook.com>
2024-06-19 14:18:20 -0500

    init

---
+++ src/toolbox/install.sh
@@ -0,0 +1,4 @@
+# Install oh-my-zsh plugins
+
+zsh -c 'git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions'
+zsh -c 'git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting'
